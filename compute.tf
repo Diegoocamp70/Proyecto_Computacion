6275e76201a85892f5685740f7b98219b877fede
Diego Ocampo <117787259+Diegoocamp70@users.noreply.github.com>
2025-10-30 19:22:28 -0500

    prueba

--- compute.tf
+++ compute.tf
@@ -91,7 +91,7 @@ resource "azurerm_linux_virtual_machine" "Maquinas_Virtuales" {
 #     username   = "adminuser"
 #     public_key = file("~/.ssh/id_rsa.pub")
 #   
-#
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
